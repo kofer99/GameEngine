cdfbdb448e779025b6a086916e5e3ca34ba25558
albrecht-flo <florian.albrecht.public@outlook.de>
2017-06-02 16:12:43 +0200

    Main class

---
+++ GameEngine/src/gameengine/Engine.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package gameengine;
+
+/**
+ * @author Florian Albrecht
+ *
+ */
+public class Engine {
+
+	public Engine(){
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Engine engine = new Engine();
+	}
+}
