7680d616b8c4e2c0b8fe945abea9df95ab0989c5
albrecht-flo <florian.albrecht.public@outlook.de>
2017-06-28 12:37:07 +0200

    Added the Entity and the EntityList

--- GameEngine/src/gameengine/collections/EntityList.java
+++ GameEngine/src/gameengine/collections/EntityList.java
@@ -18,21 +18,54 @@ public class EntityList extends ArrayList<Entity> {
 	 */
 	private static final long serialVersionUID = 6927682181843481321L;
 
+	private static int nextID = 0;
+	private static ArrayList<Integer> freeIDs = new ArrayList<Integer>(100);
+
 	/**
+	 * Adds an <code>Entity</code> and sets its id to the next free id or the
+	 * first of the deleted ids.
+	 * 
 	 * @param e
+	 *            The Entity to be added
 	 */
-	public void addEntity(Entity e) {
-		// TODO Auto-generated method stub
-		
+	public boolean addEntity(Entity e) {
+		if (freeIDs.isEmpty()) {
+			e.setID(nextID++);
+			return super.add(e);
+		} else {
+			e.setID((Integer) freeIDs.remove(freeIDs.size() - 1));
+			return setEntity(e.getID(), e);
+		}
 	}
 
 	/**
+	 * Removes an <code>Entity</code>, by setting its status to deleted and
+	 * freeing its id.
+	 * 
 	 * @param eID
+	 *            The id of the Entity
 	 * @return
 	 */
 	public Entity removeEntity(int eID) {
-		// TODO Auto-generated method stub
-		return null;
+		Entity e = super.get(eID);
+		e.setDeleted();
+		freeIDs.add(eID);
+		return e;
+	}
+
+	/**
+	 * This method is a helper method, witch will be used to improve memory
+	 * management.
+	 * 
+	 * @param i
+	 *            The id to be inserted to
+	 * @param e
+	 *            The Entity to insert
+	 * @return
+	 */
+	private boolean setEntity(int i, Entity e) {
+		super.set(i, e);
+		return true;
 	}
 
 }
--- GameEngine/src/gameengine/objects/Entity.java
+++ GameEngine/src/gameengine/objects/Entity.java
@@ -3,17 +3,54 @@
  */
 package gameengine.objects;
 
+import gameengine.Main;
+
 /**
  * @author Florian Albrecht
  *
  */
 public class Entity {
 
+	private int id;
+	private boolean deleted = false;
+
 	/**
-	 * 
+	 * Calls the Main.engine.addEntity method to add itself to the EntityHandler
+	 * and to get its id.
 	 */
 	public Entity() {
-		// TODO Auto-generated constructor stub
+		Main.engine.addEntity(this);
 	}
 
+	/**
+	 * Sets the id
+	 * 
+	 * @param id
+	 */
+	public void setID(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * 
+	 * @return The id of the Entity
+	 */
+	public int getID() {
+		return id;
+	}
+
+	/**
+	 * Sets the deleted flag to true.
+	 */
+	public void setDeleted() {
+		deleted = true;
+	}
+
+	/**
+	 * 
+	 * @return The deleted flag
+	 */
+	public boolean getDeleted() {
+		return deleted;
+	}
 }
