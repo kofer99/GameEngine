b8d754696ff63d07a7732dd543cab757f63116ff
albrecht-flo <florian.albrecht.public@outlook.de>
2017-07-15 18:23:13 +0200

    Cleanup of TestGame

--- GameEngine/game/game/TestGame.java
+++ GameEngine/game/game/TestGame.java
@@ -5,6 +5,7 @@ package game;
 
 import far.math.vec.Vec2f;
 import far.math.vec.Vec3f;
+import gameengine.components.ActionComponent;
 import gameengine.components.PhysicComponent;
 import gameengine.components.Renderable;
 import gameengine.components.Transform;
@@ -16,38 +17,33 @@ import gameengine.objects.Game;
  */
 public class TestGame implements Game {
 
-	Transform Player;
-	Player p1;
-	PhysicComponent PlPh;
-
-	Transform Player2;
-	Player2 p2;
-	PhysicComponent PlPh2;
-
 	public TestGame() {
 
 	}
 
 	@Override
 	public void init() {
-		Player = new Transform(new Vec3f(13f, 5f, 0f), new Vec2f(2f, 2f), new Vec3f(0, 0, 0));
-		PlPh = new PhysicComponent(Player, 1);
-		p1 = new Player(PlPh);
+		Entity player = new Entity();
 
-		Player2 = new Transform(new Vec3f(5f, 5f, 0f), new Vec2f(2f, 2f), new Vec3f(0, 0, 0));
+		Transform playerTransform = new Transform(new Vec3f(13f, 5f, 0f), new Vec2f(2f, 2f), new Vec3f(0, 0, 0));
+		PhysicComponent playerPhysic = new PhysicComponent(playerTransform, 1);
+		ActionComponent playerAction = new Player(playerPhysic);
 
-		PlPh2 = new PhysicComponent(Player2, 1);
-		p2 = new Player2(PlPh2);
-
-		Entity e3 = new Entity();
-		e3.add(new Renderable("Player.png", Player));
-		e3.add(PlPh);
-		e3.add(p1);
+		player.add(playerTransform);
+		player.add(new Renderable("Player.png", playerTransform));
+		player.add(playerPhysic);
+		player.add(playerAction);
 
 		Entity e = new Entity();
-		e.add(new Renderable("grass.png", Player2));
-		e.add(PlPh2);
-		e.add(p2);
+
+		Transform player2Transform = new Transform(new Vec3f(5f, 5f, 0f), new Vec2f(2f, 2f), new Vec3f(0, 0, 0));
+		PhysicComponent player2Physics = new PhysicComponent(player2Transform, 1);
+		ActionComponent player2Action = new Player2(player2Physics);
+
+		e.add(player2Transform);
+		e.add(new Renderable("Enemy.png", player2Transform));
+		e.add(player2Physics);
+		e.add(player2Action);
 
 		Transform tt = new Transform(new Vec3f(-2.0f, 0.8f, 0.0f), new Vec2f(1.0f, 1.0f), new Vec3f(0, 0, 0));
 		Text text = new Text("arial", "Test Text gg xD !", tt);
