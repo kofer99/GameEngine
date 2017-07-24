a99e8526c54c2c3c230c389167ea0564aea5f58f
albrecht-flo <florian.albrecht.public@outlook.de>
2017-07-24 12:23:50 +0200

    Update Entity class

--- GameEngine/src/gameengine/objects/Entity.java
+++ GameEngine/src/gameengine/objects/Entity.java
@@ -4,6 +4,11 @@
 package gameengine.objects;
 
 import gameengine.Main;
+import gameengine.components.ActionComponent;
+import gameengine.components.AudioComponent;
+import gameengine.components.PhysicComponent;
+import gameengine.components.Renderable;
+import gameengine.components.Transform;
 
 /**
  * This class represents a Entity, witch is only and identifier.<br>
@@ -31,8 +36,35 @@ public class Entity {
 	 * 
 	 * @param component
 	 */
+
+	public Renderable renderable;
+	public Transform transform;
+	public PhysicComponent pysics;
+	public ActionComponent action;
+	public AudioComponent audio;
+
 	public void add(Component component) {
 		component.setEntityID(id);
+		switch (component.getType()) {
+		case RENDERABLE:
+			this.renderable = (Renderable) component;
+			break;
+		case TRANSFORM:
+			this.transform = (Transform) component;
+			break;
+		case PHYSIC:
+			this.pysics = (PhysicComponent) component;
+			break;
+		case ACTION:
+			this.action = (ActionComponent) component;
+			break;
+		case AUDIO:
+			this.audio = (AudioComponent) component;
+			break;
+		default:
+			break;
+		}
+
 		Main.engine.addComponent(component);
 	}
 
